6321eccf33a17d5233af69edab48d0a6325ebb1c
asm32net <379754432@qq.com>
2018-07-22 21:59:14 +0800

    PaStarry2016JFrame__builder.bat ... add

---
+++ Java-2016/JFrame-2016/PaStarry2016JFrame.java/PaStarry2016JFrame__builder.bat
@@ -0,0 +1,28 @@
+@echo off
+
+set ProjName=PaStarry2016JFrame
+echo Begin ...
+echo --------------------
+md classes
+::md classes\res
+javac -d classes %ProjName%.java
+echo --------------------
+echo Done.
+
+if exist %ProjName%.manifest.txt del /f %ProjName%.manifest.txt
+
+echo Manifest-Version: 1.0> %ProjName%.manifest.txt
+echo Created-By: 1.4.2_12 (Sun Microsystems Inc.)>> %ProjName%.manifest.txt
+echo Main-Class: %ProjName%>> %ProjName%.manifest.txt
+
+echo Release ...
+echo --------------------
+md Release
+cd classes
+jar cvfm ..\Release\%ProjName%.jar ..\%ProjName%.manifest.txt %ProjName%.class
+cd ..
+del %ProjName%.manifest.txt
+echo --------------------
+echo Done.
+
+pause
