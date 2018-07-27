34467461218fe4a53bdabceb560bb1f9a6cb9575
asm32net <379754432@qq.com>
2018-07-27 13:13:21 +0800

    PaCalculator2016JFrame__builder.bat ... add

---
+++ Java-2016/JFrame-2016-ui/PaCalculator2016JFrame.java/PaCalculator2016JFrame__builder.bat
@@ -0,0 +1,26 @@
+@set ProjName=PaCalculator2016JFrame
+@echo Begin ...
+@echo --------------------
+@md classes
+@md classes\res
+@javac -d classes %ProjName%.java
+@echo --------------------
+@echo Done.
+
+@if exist %ProjName%.manifest.txt del /f %ProjName%.manifest.txt
+
+@echo Manifest-Version: 1.0> %ProjName%.manifest.txt
+@echo Created-By: 1.4.2_12 (Sun Microsystems Inc.)>> %ProjName%.manifest.txt
+@echo Main-Class: %ProjName%>> %ProjName%.manifest.txt
+
+@echo Release ...
+@echo --------------------
+@md Release
+@cd classes
+@jar cvfm ..\Release\%ProjName%.jar ..\%ProjName%.manifest.txt %ProjName%.class res/
+@cd ..
+@del %ProjName%.manifest.txt
+@echo --------------------
+@echo Done.
+
+@pause
