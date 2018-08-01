c2d3abce1490b332961bdb3e3f4517f86d092239
asm32net <379754432@qq.com>
2018-08-02 01:50:30 +0800

    SortDemo01.java ... add

---
+++ Java-2017/Eclipse-Java-2017-Demos/src/SortDemo01.java
@@ -0,0 +1,17 @@
+//import java.util.Arrays;
+
+public class SortDemo01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1, 23, 4, 10, 6, 3, 9};
+		//Arrays.sort(arr, 2, 6); // Ctrl + Shift + o
+		
+		for(int i: arr){
+			System.out.println(i);
+		}
+	}
+}
